f0fe062e9e198c025520f0b2a64fddd146b1da38
Wellington Souza <wellington.souza.pb@compasso.com.br>
2022-11-09 05:08:27 -0300

    Update index.tsx
    Home page structure

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,36 @@
 import React from "react";
+import Follow from "../../components/Follow/Follow";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import Menu from "../../components/Menu/Menu";
+import News from "../../components/News/News";
+import Post from "../../components/Post/Post";
+import Search from "../../components/Search/Search";
+import TweetFeed from "../../components/TweetFeed/TweetFeed";
 import "./Home.scss";
 
 export default function HomePage() {
   return (
     <main className="home-page">
-      {`Home Page`}
+      <section className="page-content">
+        <aside className="a-side">
+          <Menu/>
+        </aside>
+        <section className="main-content">
+          <Header label="Home"/>
+          <Post/>
+          <TweetFeed/>
+        </section>
+        <aside className="b-side">
+          <Search/>
+          <News/>
+          <Follow/>
+        </aside>
+      </section>
+      <Footer
+        title="Don’t miss what’s happening"
+        desc="People on Twitter are first to know."
+        />
     </main>
   );
 }
